b86835035166019222d1aa287f9f2931a10fc04c
Muhammad Naseer Aslam <naseeraslamkhan016@gmail.com>
2023-07-10 09:52:44 +0000

    Event handling in react

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,19 +3,23 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["Naseer", "Sohaib", "Basharat"],
+  };
+
+  handleIncrement = (id) => {
+    console.log(id);
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag} </li>
-          ))}
-        </ul>
+        <button
+          onClick={() => this.handleIncrement(1)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
